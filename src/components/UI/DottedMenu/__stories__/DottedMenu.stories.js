ddcf5e8793cc32c668ad7be74d5ab8472454ec63
ilia <i.makarov@andersenlab.com>
2020-02-01 22:04:59 +0200

    rename story file for DottedMenu

---
+++ src/components/UI/DottedMenu/__stories__/DottedMenu.stories.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+
+import { DottedMenu } from '../index'
+
+export default {
+  title: 'DottedMenu',
+};
+
+export const doc = () => (
+  <DottedMenu items={[
+    {
+      title: 'Item1',
+      fn: action('Item1Fn'),
+    }, {
+      title: 'Item2',
+      fn: action('Item2Fn'),
+    },
+  ]} />
+);
+
+export const withDialog = () => (<span>withDialog</span>);
